533b381749f368020b5335e86d63e11be2ab06de
Marcus <marcusnerloe@gmail.com>
2020-03-12 13:52:43 +0100

    Update

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ client.on("ready", () => {
 
         reactionMessageID = msg.id;
 
-        emotes.forEach(emote => msg.react(emote));
+        emotes.forEach(emote => console.log(emote)); // msg.react(emote)
       });
   });
 });
