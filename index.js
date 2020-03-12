8c7df4be067107c99883a1f4a0efa8f7a45a433b
Marcus <marcusnerloe@gmail.com>
2020-03-12 15:04:04 +0100

    Update

--- index.js
+++ index.js
@@ -12,12 +12,14 @@ async function clear(channel) {
 }
 
 var ready = false;
-var reactionMessageID;
+var reactionMessageIDs = [];
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
   client.guilds.cache.forEach(guild => {
-    const channel = guild.channels.cache.get("687625065608642560");
+    const channel = guild.channels.cache.find(
+      channel => channel.name == "klasse-roller"
+    );
     clear(channel);
 
     channel
@@ -25,13 +27,13 @@ client.on("ready", () => {
         "@everyone Reagér på denne besked med følgende emojis, og i vil herefter modtage ranks som passer til jeres klasse og årgang."
       )
       .then(msg => {
-        //console.log("emojis", msg.guild.emojis.cache);
         const emotes = [];
         msg.guild.emojis.cache.forEach(emoji => {
           if (
             emoji.name === "1Q" ||
             emoji.name === "1X" ||
             emoji.name === "1T" ||
+            emoji.name === "1P" ||
             emoji.name === "3Q" ||
             emoji.name === "3X"
           ) {
@@ -39,7 +41,7 @@ client.on("ready", () => {
           }
         });
 
-        reactionMessageID = msg.id;
+        reactionMessageIDs.push(msg.id);
 
         emotes.forEach(emote => msg.react(emote));
         ready = true;
@@ -48,16 +50,16 @@ client.on("ready", () => {
 });
 
 client.on("messageReactionAdd", (reaction, user) => {
-  if (user.id == "687618530283225168") return;
+  if (user.id == client.user.id) return;
 
-  if (reaction.message.id == reactionMessageID) {
+  if (reactionMessageIDs.includes(reaction.message.id)) {
     //console.log(messageReaction);
     var emoji = reaction.message.guild.emojis.cache.find(
       emoji => emoji.name === reaction._emoji.name
     );
 
     if (!emoji) {
-      user.send("**FEjL!** Kunne ikke finde nogen rolle med denne emoji!");
+      user.send("**FEJL!** Kunne ikke finde nogen rolle med denne emoji!");
       reaction.remove();
       return;
     }
@@ -67,7 +69,7 @@ client.on("messageReactionAdd", (reaction, user) => {
     );
 
     if (!role) {
-      user.send("**FEjL!** Kunne ikke finde nogen rolle med denne emoji!");
+      user.send("**FEJL!** Kunne ikke finde nogen rolle med denne emoji!");
       reaction.remove();
       return;
     }
@@ -80,11 +82,12 @@ client.on("messageReactionAdd", (reaction, user) => {
           (gRole.name == "1Q" ||
             gRole.name == "1T" ||
             gRole.name == "1X" ||
+            gRole.name == "1P" ||
             gRole.name == "3Q" ||
             gRole.name == "3X")
       )
     ) {
-      user.send("**FEjL!** Du har allerede modtaget en klasserolle!");
+      user.send("**FEJL!** Du har allerede modtaget en klasserolle!");
       reaction.users.remove(user);
       return;
     }
@@ -95,16 +98,15 @@ client.on("messageReactionAdd", (reaction, user) => {
 });
 
 client.on("messageReactionRemove", (reaction, user) => {
-  if (user.id == "687618530283225168") return;
+  if (user.id == client.user.id) return;
 
   if (ready || reaction.message.id == reactionMessageID) {
-    //console.log(messageReaction);
     var emoji = reaction.message.guild.emojis.cache.find(
       emoji => emoji.name === reaction._emoji.name
     );
 
     if (!emoji) {
-      user.send("**FEjL!** Kunne ikke finde nogen rolle med denne emoji!");
+      user.send("**FEJL!** Kunne ikke finde nogen rolle med denne emoji!");
       return;
     }
 
@@ -113,14 +115,13 @@ client.on("messageReactionRemove", (reaction, user) => {
     );
 
     if (!role) {
-      user.send("**FEjL!** Kunne ikke finde nogen rolle med denne emoji!");
+      user.send("**FEJL!** Kunne ikke finde nogen rolle med denne emoji!");
       return;
     }
 
-    //console.log(role);
-    //user.send("Du har nu fået fjernet klasserollen: **" + role.name + "**");
-
     let gMember = reaction.message.guild.member(user);
+
+    //user.send("Du har nu fået fjernet klasserollen: **" + role.name + "**");
     gMember.roles.remove(role);
   }
 });
